eadc68bb982a9257f74f9c6fea3578c62ab7f4f5
HeZhaohui <2311719626@qq.com>
2025-04-26 10:50:38 +0800

    feat:Sping AI 调用测试

---
+++ src/main/java/com/hezhaohui/aiagent/demo/invoke/SpringAiAiInvoke.java
@@ -0,0 +1,27 @@
+package com.hezhaohui.aiagent.demo.invoke;
+
+import com.alibaba.dashscope.assistants.AssistantParam;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring AI 框架配置 AI 大模型
+ */
+@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage assistantMessage =  dashscopeChatModel.call(new Prompt("你爱玩原神吗"))
+                .getResult()
+                .getOutput();
+        System.out.println(assistantMessage.getText());
+    }
+}
